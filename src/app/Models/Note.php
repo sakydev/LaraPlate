88630a4a2afe38011f79fc683d7c2f7ed9ea3c13
sakydev <saqibrzzaq@gmail.com>
2024-12-25 18:38:54 +0500

    Add Note fields annot

--- src/app/Models/Note.php
+++ src/app/Models/Note.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $content
+ * @property string $status
+ * @property int $user_id
+ *
+ * @property string $created_at
+ * @property string $updated_at
+ * */
 class Note extends Model
 {
     use HasFactory;

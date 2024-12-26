fe7beac3f80bcb1723bb133f55a656bc609ec916
sakydev <saqibrzzaq@gmail.com>
2024-12-26 18:29:59 +0500

    use constant for response code

--- src/app/Resources/Api/V1/Responses/SuccessResponse.php
+++ src/app/Resources/Api/V1/Responses/SuccessResponse.php
@@ -3,6 +3,7 @@
 namespace App\Resources\Api\V1\Responses;
 
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class SuccessResponse extends JsonResponse
 {
@@ -13,7 +14,7 @@ class SuccessResponse extends JsonResponse
     public function __construct(
         string $message,
         array $content = [],
-        int $status = 200,
+        int $status = Response::HTTP_OK,
         array $headers = [],
         int $options = 0,
     ) {

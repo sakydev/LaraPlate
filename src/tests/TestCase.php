dac1b3fa74a7c3cdabe1d3200a11647baf053bef
sakydev <saqibrzzaq@gmail.com>
2024-12-26 18:36:28 +0500

    fix phpstan errors

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -6,12 +6,18 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * @param array<string, mixed> $response
+     * */
     public function assertSuccess(array $response): void
     {
         static::assertTrue($response['success']);
         static::assertNull($response['errors']);
     }
 
+    /**
+     * @param array<string, mixed> $response
+     * */
     public function assertError(array $response): void
     {
         static::assertNull($response['success']);
